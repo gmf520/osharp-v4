57a89282d9ef0751d952f75c6e2e86f370a04cb2
hunji <guo194194@163.com>
2015-10-13 13:44:39 +0800

    测试wcf

---
+++ developing/RK.TZ.Core/Contracts/IDepartmentContract.cs
@@ -0,0 +1,15 @@
+﻿using OSharp.Core.Dependency;
+using RK.TZ.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RK.TZ.Core.Contracts
+{
+    public interface IDepartmentContract: ILifetimeScopeDependency
+    {
+         IQueryable<Department> GetInfos();
+    }
+}
