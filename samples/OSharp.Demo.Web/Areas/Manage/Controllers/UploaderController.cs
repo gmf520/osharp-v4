608040b068cb4277e7879e64d6163d8ab94db5c7
jesn2013@hotmail.com <visual2013>
2015-10-13 18:07:32 +0800

    添加 webuploader 控件相关文件

---
+++ samples/OSharp.Demo.Web/Areas/Manage/Controllers/UploaderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OSharp.Demo.Web.Areas.Manage.Controllers
+{
+    public class UploaderController : Controller
+    {
+        // GET: Manage/Uploader
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
