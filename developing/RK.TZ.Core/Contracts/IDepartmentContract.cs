86afdaf31bff242004317d9ee18dafe55e0240f3
hunji <guo194194@163.com>
2015-10-14 10:41:31 +0800

    跑通系统，尝试wcf

--- developing/RK.TZ.Core/Contracts/IDepartmentContract.cs
+++ developing/RK.TZ.Core/Contracts/IDepartmentContract.cs
@@ -3,13 +3,16 @@ using RK.TZ.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RK.TZ.Core.Contracts
 {
+    [ServiceContract]
     public interface IDepartmentContract: ILifetimeScopeDependency
     {
+         [OperationContract]
          IQueryable<Department> GetInfos();
     }
 }
---
+++ developing/RK.TZ.Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RK.TZ.Service.Global" Language="C#" %>
