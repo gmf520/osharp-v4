19bd34e884a97bb5c388c51bb96b72ee0e25eb37
jesn2013@hotmail.com <visual2013>
2015-10-16 14:51:26 +0800

    创建自己的分支防止在合并版本的时候原作者的覆盖自己的项目分支

---
+++ samples/OSharp.Demo.Web/Areas/BootStrap/BootStrapAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace OSharp.Demo.Web.Areas.BootStrap
+{
+    public class BootStrapAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "BootStrap";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "BootStrap_default",
+                "BootStrap/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
