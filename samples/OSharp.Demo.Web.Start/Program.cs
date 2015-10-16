ba18fca0acb28256c32d830e4362190b6fb60cb3
龙江 <x120039483@hotmail.com>
2015-10-16 22:41:19 +0800

    增加一个Owin启动控制台，用来启动OSharp.Demo.Web

---
+++ samples/OSharp.Demo.Web.Start/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace OSharp.Demo.Web.Start
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("启动中...\n");
+
+            string baseAddress = "http://localhost:9001/"; // 管道地址
+            //开启管道监听
+            WebApp.Start<OSharp.Demo.Web.Startup>(url: baseAddress);
+
+            Console.WriteLine("启动成功！\n");
+            Console.WriteLine("访问地址：{0} \n", baseAddress);
+
+            Console.WriteLine("按任意键结束...");
+            Console.ReadKey();
+        }
+    }
+}
