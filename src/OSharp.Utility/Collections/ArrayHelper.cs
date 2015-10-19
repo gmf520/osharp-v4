3c40fc779eaa86e0b946bd0cc12c8725bb2b8b63
龙江 <x120039483@hotmail.com>
2015-10-19 23:11:46 +0800

    判断对象是否为数组的一个类库

---
+++ src/OSharp.Utility/Collections/ArrayHelper.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ArrayHelper.cs" company="OSharp开源团队">
+//      Copyright (c) 2014-2015 OSharp. All rights reserved.
+//  </copyright>
+//  <site>http://www.osharp.org</site>
+//  <last-editor>谢龙江</last-editor>
+//  <last-date>2015-10-18 8:53</last-date>
+// -----------------------------------------------------------------------
+
+using System;
+
+
+namespace OSharp.Utility.Collections
+{
+    /// <summary>
+    /// 判断对象是否为数组
+    /// </summary>
+    public static class ArrayHelper
+    {
+        public static bool IsArray(this object o)
+        {
+            if (null == o) return false;
+            return o.GetType().BaseType == typeof(Array);
+        }
+    }
+}
