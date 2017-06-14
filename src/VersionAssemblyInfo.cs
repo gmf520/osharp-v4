970e1b39c1aa8ecd745a550d04a0c6f1efcd0e78
郭明锋 <gmf520@yeah.net>
2017-06-14 14:41:43 +0800

    更新版本号到4.1，发布nuget程序集

--- src/VersionAssemblyInfo.cs
+++ src/VersionAssemblyInfo.cs
@@ -13,5 +13,5 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("4.0")]
-[assembly: AssemblyFileVersion("4.0.30")]
-[assembly: AssemblyInformationalVersion("4.0.30")]
+[assembly: AssemblyFileVersion("4.1")]
+[assembly: AssemblyInformationalVersion("4.1")]
