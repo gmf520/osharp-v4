a1a25e5a5f5d2b7b5487d0542b0760710d7198c1
郭明锋 <gmf520@yeah.net>
2018-03-18 14:19:02 +0800

    修复Repository的CheckExists功能，当Key为引用类型时引发空引用异常的问题

--- src/OSharp.Data.Entity/Repository.cs
+++ src/OSharp.Data.Entity/Repository.cs
@@ -475,7 +475,7 @@ namespace OSharp.Data.Entity
             predicate.CheckNotNull("predicate");
             TKey defaultId = default(TKey);
             var entity = _dbSet.Where(predicate).Select(m => new { m.Id }).FirstOrDefault();
-            bool exists = (!typeof(TKey).IsValueType && id.Equals(null)) || id.Equals(defaultId)
+            bool exists = (!typeof(TKey).IsValueType && ReferenceEquals(id, null)) || id.Equals(defaultId)
                 ? entity != null
                 : entity != null && !entity.Id.Equals(id);
             return exists;
@@ -955,7 +955,7 @@ namespace OSharp.Data.Entity
             predicate.CheckNotNull("predicate");
             TKey defaultId = default(TKey);
             var entity = await _dbSet.Where(predicate).Select(m => new { m.Id }).FirstOrDefaultAsync();
-            bool exists = (!(typeof(TKey).IsValueType) && id.Equals(null)) || id.Equals(defaultId)
+            bool exists = (!(typeof(TKey).IsValueType) && ReferenceEquals(id, null)) || id.Equals(defaultId)
                 ? entity != null
                 : entity != null && !entity.Id.Equals(id);
             return exists;
